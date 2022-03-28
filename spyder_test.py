dbf4ecc7fd892ee60862befd8bba1f1a6e0b3d73
Carla Arellano <93019319+cmmarellano@users.noreply.github.com>
2022-03-28 11:18:36 +0200

    Add files via upload

---
+++ spyder_test.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Dies ist eine temporäre Skriptdatei.
+"""
+#import os
+#import sys
+#print(sys.path)
+
+#test test test test
+
+#help('modules')
+
+
+
+# variable definition
+days_of_the_week = 7
+
+
+
+
+def say_hello(recipient):
+    print('Hello, world! Hello {}'.format(recipient))
+    return recipient      
+
+
